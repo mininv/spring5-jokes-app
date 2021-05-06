18adc9ae0382fa4e9d62d59204e452938c9ed658
mininv <vaiermolenko@gmail.com>
2021-05-06 11:50:43 +0300

    add services and thymeleaf html page

---
+++ src/main/java/ru/yarm/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package ru.yarm.spring5jokesapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.yarm.spring5jokesapp.services.JokesService;
+
+@Controller
+public class JokeController {
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/ru/yarm/spring5jokesapp/services/JokesService.java
@@ -0,0 +1,5 @@
+package ru.yarm.spring5jokesapp.services;
+
+public interface JokesService {
+    String getJoke();
+}
